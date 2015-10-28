b37ea5d07e95ddddee0e625e68f01078fafce025
fengor wolfsclaw <fengor@fengors-realm.de>
2015-10-28 06:40:20 +0100

    removed hardcoded output file. Output now goes to stdout and can be redirected with > this allows the script to be used from within vim via :read

--- ReadMe.md
+++ ReadMe.md
@@ -3,11 +3,13 @@ bookshelf.py
 
 This tool takes a list of ISBNs and converts them to a html bookshelf. Jinja2 is used as the templating engine.
 
-It reads either a given file or from stdin.
+It reads either a given file or from stdin and outputs to stdout
 
 Usage
 -----
 
-$ ./bookshelf.py isblist.txt
+from commandline:
+$ ./bookshelf.py isbnlist.txt > output.html
 
-
+or from within vim:
+:read !./bookshelf.py isbnlist.txt
--- bookshelf.py
+++ bookshelf.py
@@ -5,14 +5,12 @@ import fileinput
 
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader('.'))
+env = Environment(loader=FileSystemLoader('.'), trim_blocks=True)
 template = env.get_template('shelf.j2')
 
 booklist = []
 
 for line in fileinput.input():
-    print "Looking up ISBN: %s" % line
     booklist.append(isbnlib.meta(line, 'openl'))
 
-with open('shelf.html', 'wb') as output_file:
-    output_file.write(template.render(books=booklist))
+print template.render(books=booklist),

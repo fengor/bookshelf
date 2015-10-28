8d1409f5fcc6777292052f01783c228dbc86f50a
fengor wolfsclaw <fengor@fengors-realm.de>
2015-10-28 22:01:57 +0100

    Implemented -t TEMPLATE and -s SERVICE switches

--- ReadMe.md
+++ ReadMe.md
@@ -1,9 +1,15 @@
 bookshelf.py
 ============
 
-This tool takes a list of ISBNs and converts them to a html bookshelf. Jinja2 is used as the templating engine.
+This tool takes a list of ISBNs and converts them to a html bookshelf. Jinja2 is used as the templating engine and isbnlib as the library for the actual lookup..
 
 It reads either a given file or from stdin and outputs to stdout
+
+Commandline switches
+--------------------
+
+-s, --service: the service to use for the lookup (default: openl)
+-t, --template: the template to use (default: shelf.j2)
 
 Usage
 -----
--- bookshelf.py
+++ bookshelf.py
@@ -1,16 +1,36 @@
 #! /usr/bin/python2
 
 import isbnlib
-import fileinput
+import argparse
+import sys
 
 from jinja2 import Environment, FileSystemLoader
 
+parser = argparse.ArgumentParser()
+parser.add_argument('isbnfile', type=file,
+                    help='file to load ISBNs from',
+                    nargs='?')
+parser.add_argument('-t', '--template',
+                    help='Choose template to use',
+                    default='shelf.j2')
+parser.add_argument('-s', '--service',
+                    help='Choose service to use',
+                    choices=['merge', 'wcat', 'goob', 'openl'],
+                    default='openl')
+args = parser.parse_args()
+
+print args
+
 env = Environment(loader=FileSystemLoader('.'), trim_blocks=True)
-template = env.get_template('shelf.j2')
+template = env.get_template(args.template)
 
 booklist = []
 
-for line in fileinput.input():
-    booklist.append(isbnlib.meta(line, 'openl'))
+if args.isbnfile is not None:
+    for line in args.isbnfile:
+        booklist.append(isbnlib.meta(line, args.service))
+else:
+    for line in sys.stdin:
+        booklist.append(isbnlib.meta(line, args.service))
 
 print template.render(books=booklist),
